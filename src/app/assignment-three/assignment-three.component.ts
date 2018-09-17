95794312fdea656798c7ed7ecd02d0ab11ad223a
Nikos Giannopoulos <n.giannopoulos@jrtechnologies.com>
2018-09-17 16:56:27 +0300

    Assignment 3 preparation

---
+++ src/app/assignment-three/assignment-three.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-three',
+  templateUrl: './assignment-three.component.html',
+  styleUrls: ['./assignment-three.component.css']
+})
+export class AssignmentThreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

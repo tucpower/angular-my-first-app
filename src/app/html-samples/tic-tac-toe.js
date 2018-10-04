acc35b152a71fc36e394b56b048c1c30b144c109
Nikos Giannopoulos <n.giannopoulos@jrtechnologies.com>
2018-10-04 17:08:46 +0300

    After lecture 132.

--- src/app/html-samples/tic-tac-toe.js
+++ src/app/html-samples/tic-tac-toe.js
@@ -1,3 +1,29 @@
-var name = prompt("Name?");
-alert("Nice to meet you, " + name);
-console.log("Nice to meet you, " + name);
+// var secretNumber = 4;
+
+// var number = Number(prompt("Guess a number!"));
+
+// if(number > secretNumber) {
+//     alert("Wrong number...over...");
+// } else if(number < secretNumber) {
+//     alert("Wrong number...below...");
+// } else {
+//     alert("Cool!!!");
+// }
+
+// console.log("Nice to meet you, " + name);
+
+// for (let index = 0; index < 20; index++) {
+//     console.log(index);
+    
+// }
+
+function isEven(x) {
+    if(x % 2 === 0)
+        return "yes!"
+    else
+        return "no!"
+}
+
+var number = Number(prompt("Enter number!"));
+alert(isEven(number));
+

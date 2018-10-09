98078c29bc3936e50d3b15e922d4abe4973b3086
Nikos Giannopoulos <n.giannopoulos@jrtechnologies.com>
2018-10-09 16:46:28 +0300

    After lecture 170.

--- src/app/html-samples/tic-tac-toe.js
+++ src/app/html-samples/tic-tac-toe.js
@@ -1,59 +1,23 @@
-// var secretNumber = 4;
-
-// var number = Number(prompt("Guess a number!"));
-
-// if(number > secretNumber) {
-//     alert("Wrong number...over...");
-// } else if(number < secretNumber) {
-//     alert("Wrong number...below...");
-// } else {
-//     alert("Cool!!!");
-// }
-
-// console.log("Nice to meet you, " + name);
-
-// for (let index = 0; index < 20; index++) {
-//     console.log(index);
-    
-// }
-
-// function isEven(x) {
-//     if(x % 2 === 0)
-//         return "yes!"
-//     else
-//         return "no!"
-// }
-
-// var number = Number(prompt("Enter number!"));
-// alert(isEven(number));
-
-// var nextAction = "";
-// var todos = ["111", "222", "333"];
-// todos.forEach(function (todo, i) {
-//     console.log(todo);
-//     console.log(i);
-// })
-
-var movie1 = {
-    title : "In Bruges",
-    rating : 5,
-    hasWatched : true
-}
-var movie2 = {
-    title : "Frozen",
-    rating : 4.5,
-    hasWatched : false
-}
-
-var movies = [movie1, movie2];
-movies.forEach(movie => {
-    if(movie.hasWatched)
-        console.log("You have watched \"" + movie.title + "\" - " + movie.rating + " stars");
-    else
-        console.log("You have not watched \"" + movie.title + "\" - " + movie.rating + " stars");
-});
-
-///////////////////////////////////// 153.
-
 var tag = document.getElementById('highlight');
 tag.style.color="yellow";
+
+var playerOneButton = document.getElementById("p1");
+var playerTwoButton = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
+var playerOneScore = document.getElementById("playerOneScore");
+var playerTwoScore = document.getElementById("playerTwoScore");
+
+var p1score = 0;
+var p2score = 0;
+
+playerOneButton.addEventListener("click", function() {
+    playerOneScore.textContent = ++p1score;
+})
+
+playerTwoButton.addEventListener("click", function() {
+    playerTwoScore.textContent = ++p2score;
+})
+
+resetButton.addEventListener("click", function() {
+    
+})
